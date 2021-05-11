1b5797d6cc8c579d2439b09c9330b6c5dab0cab1
Makihara <clmakihara@gmail.com>
2021-05-11 19:44:47 -0300

    Commiti inicial

---
+++ ApiDotNet/Migrations/20210511182850_initialcreate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiDotNet.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categorias",
+                newName: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Categorias",
+                newName: "Id");
+        }
+    }
+}
---
+++ ApiDotNet/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDotNet.Models
+{
+    public class Categoria
+    {
+
+        public int id { set; get; }
+       // [Display(Name = "Descrição")]
+      //  [Required(ErrorMessage = "O campo descrição obrigatório")]
+        //[Range(1,10,ErrorMessage ="Valor errada")]
+        public string Descricao { set; get; }
+
+       // public List<Produto> Produtos { get; set; }
+
+    }
+}
